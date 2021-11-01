d3f0e16565ec82649f55ff21848a8fa1ccf4bd45
Clifford <kingsleyclifford2016@gmail.com>
2021-11-01 21:28:04 +0000

    added lines of code

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,34 +1,58 @@
 import React from "react";
 import image7 from "../images/image7.jpg";
-import image8 from "../images/image8.jpg";
 import image9 from "../images/image9.jpg";
 import image10 from "../images/image10.jpg";
 import image11 from "../images/image11.jpg";
 import image12 from "../images/image12.jpg";
 import image13 from "../images/image13.jpg";
+import image14 from "../images/image14.jpg";
+import image15 from "../images/image15.jpg";
+import image16 from "../images/image16.jpg";
+import image17 from "../images/image17.jpg";
+import image18 from "../images/image18.jpg";
 
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/swiper-bundle.min.css";
-import "swiper/swiper.min.css";
+import Slider from "react-slick";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function Gallery() {
-  const imageArr = [image7, image8, image9, image10, image11, image12, image13];
+  const imageArr = [
+    image7,
+    image9,
+    image10,
+    image11,
+    image12,
+    image13,
+    image14,
+    image15,
+    image16,
+    image17,
+    image18,
+  ];
+
+  const settings = {
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 2000,
+    cssEase: "linear",
+  };
   return (
-    <div className="px-12 mt-18 mb-10">
-      <Swiper
-        spaceBetween={50}
-        slidesPerView={3}
-        onSlideChange={() => console.log("slide change")}
-        onSwiper={(swiper) => console.log(swiper)}
-      >
-        {imageArr.map((image) => (
-          <SwiperSlide>
-            <div className="w-96">
-              <img src={image} alt="" />
-            </div>
-          </SwiperSlide>
+    <div className="px-12 mt-16 mb-10">
+      <Slider {...settings}>
+        {imageArr.map((image, index) => (
+          <div key={index} className="w-96 h-80 shadow-lg">
+            <img
+              src={image}
+              className="h-full w-full object-cover rounded-lg"
+              alt=""
+            />
+          </div>
         ))}
-      </Swiper>
+      </Slider>
     </div>
   );
 }
